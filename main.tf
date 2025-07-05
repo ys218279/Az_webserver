41dc191920a23a215694bafae1c4eded60df41cf
ys218279 <ys218279@gmail.com>
2025-07-05 15:58:38 +0100

    Restarting so that I can follow new methodology starting with a bootstrap phase

--- main.tf
+++ main.tf
@@ -19,33 +19,3 @@ resource "azurerm_resource_group" "main" {
   location = var.location
   tags     = var.tags
 }
-
-
-# -------------------------------------------
-# Create Virtual Network
-# -------------------------------------------
-
-resource "azurerm_virtual_network" "main" {
-  name = var.vnet_name
-  resource_group_name = azurerm_resource_group.main.name
-  location = var.location
-  address_space = var.address_space
-  tags = var.tags
-}
-
-resource "azurerm_subnet" "main" {
-  name = var.subnet_name
-  resource_group_name = azurerm_resource_group.main.name
-  virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes = [var.subnet_prefix]
-}
-# -------------------------------------------
-# Jump Box Subnet
-# -------------------------------------------
-
-resource "azurerm_subnet" "jumpbox" {
-  name                 = var.jumpbox_subnet_name
-  resource_group_name  = azurerm_resource_group.main.name
-  virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = [var.jumpbox_subnet_prefix]
-}
